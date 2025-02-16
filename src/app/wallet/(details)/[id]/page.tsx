0ea88f48fec50be30f35912d840ce8803358d96b
Joel Victor <jnull.rx@gmail.com>
2025-02-15 23:34:18 -0300

    PPI-36: fix -- Adicionando log para análisar problema

--- src/app/wallet/(details)/[id]/page.tsx
+++ src/app/wallet/(details)/[id]/page.tsx
@@ -6,6 +6,7 @@ const walletService = new WalletService();
 
 export default function WalletDetailsPage({ params }: { params: Promise<{ id: string }> }) {
     const { id } = use(params);
+    console.log("id", id);
     const wallet = walletService.getById(id);
 
     return (
